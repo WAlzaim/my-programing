5c623ffbca7476765734bc35d713b39f446da40c
WAlzaim <watenalzaim@gmail.com>
2026-01-19 15:02:45 -0500

    Add files via upload

---
+++ twopmlab1.pywaten.py
@@ -0,0 +1,15 @@
+def myHello_worl():
+    print("hello Montreal")
+
+myHello_worl()
+
+def input_output():
+    name=input("Enter your name:")
+    age=int(input("Enter your age:"))
+    height=float(input("Enter your height:(in meters)"))
+   
+    print(f"Hello {name}")
+    print(f"You are {age} years old")
+    print (f"your height is {height} meters")
+
+input_output()
